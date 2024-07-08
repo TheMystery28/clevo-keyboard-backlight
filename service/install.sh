ef79db0ae70086fbcfa15b13e611d69cbea944c3
TheMystery28 <46329375+TheMystery28@users.noreply.github.com>
2024-07-08 04:36:07 +0200

    Update install.sh

--- service/install.sh
+++ service/install.sh
@@ -1,28 +1,22 @@
 #!/usr/bin/env bash
 
-INSTALL_DIR=/var/lib/kb_light_stats
+INSTALL_DIR=/usr/lib/kb_light_stats
 CONFIG_DIR=/etc/kb_light_stats
 
-if [ ! -d "$INSTALL_DIR" ]; then
-	mkdir -p $INSTALL_DIR
-fi
+# Create directories
+mkdir -p $INSTALL_DIR
+mkdir -p $INSTALL_DIR/stats
+mkdir -p $INSTALL_DIR/TuxedoWmi
+mkdir -p $CONFIG_DIR
+
+# Copy files
 cp -v ./kb_light_stats.conf $INSTALL_DIR/kb_light_stats.conf.template
-cp -v ./kb_light_stats.service /lib/systemd/system/
+cp -v ./kb_light_stats.service /usr/lib/systemd/system/
 cp -v ./*.py $INSTALL_DIR/
-
-if [ ! -d "$INSTALL_DIR/stats" ]; then
-	mkdir -p $INSTALL_DIR/stats
-fi
 cp -vR ./stats/*.py $INSTALL_DIR/stats/
-
-if [ ! -d "$INSTALL_DIR/TuxedoWmi" ]; then
-	mkdir -p $INSTALL_DIR/TuxedoWmi
-fi
 cp -vR ./TuxedoWmi/*.py $INSTALL_DIR/TuxedoWmi/
 
-if [ ! -d "$CONFIG_DIR" ]; then
-	mkdir -p $CONFIG_DIR
-fi
+# Copy config file if it doesn't exist
 if [ ! -f "$CONFIG_DIR/kb_light_stats.conf" ]; then
-	cp -v ./kb_light_stats.conf $CONFIG_DIR/kb_light_stats.conf	
+    cp -v ./kb_light_stats.conf $CONFIG_DIR/kb_light_stats.conf    
 fi
